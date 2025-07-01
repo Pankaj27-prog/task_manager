54d039baeef21fef1e26f915ca1a594d06b061a0
Pankaj Kumar <https://github.com/PankajKumar2310/tax-loss-app.git>
2025-07-01 14:09:28 +0530

    Serve React build from Express and add catch-all route

--- server/server.js
+++ server/server.js
@@ -100,6 +100,14 @@ app.delete("/entries/:id", async (req, res) => {
   }
 });
 
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, "../build")));
+
+// Catch-all handler to serve React's index.html for any non-API route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
