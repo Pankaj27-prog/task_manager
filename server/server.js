aa425f3a4aba09f9ed6b571748074ec1fa2bd834
Pankaj Kumar <https://github.com/PankajKumar2310/tax-loss-app.git>
2025-07-01 14:15:58 +0530

    Fix catch-all route pattern for Render deployment

--- server/server.js
+++ server/server.js
@@ -104,7 +104,7 @@ app.delete("/entries/:id", async (req, res) => {
 app.use(express.static(path.join(__dirname, "../build")));
 
 // Catch-all handler to serve React's index.html for any non-API route
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
